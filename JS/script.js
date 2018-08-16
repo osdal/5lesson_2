c52aa8c33a02ade84a5c80b9c92c054980bed9d6
osdal <osdal67@gmail.com>
2018-08-16 16:52:15 +0300

    1

---
+++ JS/script.js
@@ -0,0 +1,39 @@
+﻿let openShop = document.getElementById('open-btn'),
+	leftMenu = document.getElementsByClassName('main-info'),
+	leftTitles = [],
+	leftClasses = [];
+/*	
+for (let i = 0; i < leftMenu[0].childNodes.length; i++) {
+	leftClasses.push(leftMenu[0].childNodes[i].classList);
+console.log(leftMenu[0].childNodes[i].classList);
+
+	if(leftMenu[0].childNodes[i].classList.indexOf('-')) {
+		console.log(leftMenu[0].childNodes[i].classList)
+	}
+	
+}
+*/
+let goodsIten = document.getElementsByClassName('goods-item');
+
+
+let buttons = document.getElementsByTagName('button'),
+	btnResalt =[];
+
+for (let i = 0; i < buttons.length; i++) {
+	if (i != 0) {btnResalt.push(buttons[i])}
+}
+
+let items = document.querySelector('#items');
+let time = document.querySelector('#time');
+let budget = document.querySelector('#budget');
+let employers = document.querySelectorAll('.hire-employers-item');
+
+console.log(employers);
+
+
+	/*if (leftMenu[0].childNodes[i].classList !== null){
+		  leftTitles.push(leftMenu[0].childNodes[i]) 
+		console.log(leftMenu[0].childNodes[i]);
+	}*/
+
+
